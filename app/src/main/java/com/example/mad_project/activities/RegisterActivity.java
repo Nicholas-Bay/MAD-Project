7ec0ce5d93715778dd47e7b4ef90271832f018dc
ephra <ephraimyee@gmail.com>
2022-01-17 11:06:16 +0800

    comment the section for RegisterActivity.java

--- app/src/main/java/com/example/mad_project/activities/RegisterActivity.java
+++ app/src/main/java/com/example/mad_project/activities/RegisterActivity.java
@@ -32,10 +32,10 @@ public class RegisterActivity extends AppCompatActivity {
         et_contact = findViewById(R.id.register_contact);
         et_email = findViewById(R.id.register_email);
         et_password = findViewById(R.id.register_password);
-        Button regBtn = findViewById(R.id.register_buyer);
+        Button button_register_buyer = findViewById(R.id.register_buyer);
         Button button_register_seller = findViewById(R.id.register_seller);
         // save data to firebase and update firmware
-        regBtn.setOnClickListener(v -> {
+        button_register_buyer.setOnClickListener(v -> {
             rootNode = FirebaseDatabase.getInstance();
             reference = rootNode.getReference("Users");
             reference.setValue("First data storage");
@@ -48,24 +48,29 @@ public class RegisterActivity extends AppCompatActivity {
             UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNo, password);
             reference.child(phoneNo).setValue(helperClass);
         });
-//        button_register_seller.setOnClickListener(onRegister);
+        button_register_seller.setOnClickListener(onRegister);
     }
 
     public boolean checkAll(String username, String name, String contact,
                             String email, String pwd) {
         String error = "";
         Pattern special_char = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        //error for username
         if (TextUtils.isEmpty(username)) error += "Empty Username\n";
         if (TextUtils.isEmpty(name)) error += "Empty Name\n";
+        //error for contact
         if (TextUtils.isEmpty(contact)) error += "Empty contact\n";
         else if (!Patterns.PHONE.matcher(contact).matches()) error += "Invalid Contact\n";
         else if (contact.length()!=8) error += "Invalid SG Phone Number\n";
+        //error for email
         if (TextUtils.isEmpty(email)) error += "Empty Email\n";
         else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) error += "Invalid Email\n";
+        //error for password
         if (TextUtils.isEmpty(pwd)) error += "Empty Password\n";
         else if (pwd.length() < 8) error += "Set a better Password. Minimum 8 in length\n";
         else if (!special_char.matcher(pwd).find()) error += "Password must have at least a special character\n";
-        if (error.equals("")) return true;
+
+        if (error.equals("")) return true;//no error returns true
         else {
             Toast.makeText(RegisterActivity.this, error, Toast.LENGTH_SHORT).show();
             return false;
@@ -74,7 +79,7 @@ public class RegisterActivity extends AppCompatActivity {
     public View.OnClickListener onRegister = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (checkAll(
+            if (checkAll(//runs subsequent code if every data field is key in correctly
                     et_username.getText().toString(),
                     et_name.getText().toString(),
                     et_contact.getText().toString(),

e9ab79693a2849817339aa0408ddf3c71c31ce38
ephra <ephraimyee@gmail.com>
2022-01-30 18:45:03 +0800

    RegisterActivity modification so that New username created will not overwrite the existing one WIP: SellerAdd -> added spinner

--- app/src/main/java/com/example/mad_project/activities/RegisterActivity.java
+++ app/src/main/java/com/example/mad_project/activities/RegisterActivity.java
@@ -1,16 +1,27 @@
 package com.example.mad_project.activities;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.example.mad_project.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -36,37 +47,14 @@ public class RegisterActivity extends AppCompatActivity {
         Button button_register_seller = findViewById(R.id.register_seller);
         // save Buyer data to firebase and update firmware
         button_register_buyer.setOnClickListener(v -> {
-            rootNode = FirebaseDatabase.getInstance();
-            reference = rootNode.getReference("Buyer");
-            // get all the values
-            String name = et_name.getText().toString();
-            String username = et_username.getText().toString();
-            String email = et_email.getText().toString();
-            String phoneNo = et_contact.getEditableText().toString();
-            String password = et_password.getEditableText().toString();
-            UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNo, password);
-            reference.child(username).setValue(helperClass);
-            Toast.makeText(RegisterActivity.this,"Successfully Registered as Buyer",Toast.LENGTH_LONG).show();
+            register("Buyer");
         });
         // save Seller data to firebase and update firmware
         button_register_seller.setOnClickListener(v -> {
-            rootNode = FirebaseDatabase.getInstance();
-            reference = rootNode.getReference("Seller");
-            // get all the values
-            String name = et_name.getText().toString();
-            String username = et_username.getText().toString();
-            String email = et_email.getText().toString();
-            String phoneNo = et_contact.getEditableText().toString();
-            String password = et_password.getEditableText().toString();
-            UserHelperClass helperClass = null;
-            if (checkAll(username, name, phoneNo, email, password)) {
-                helperClass = new UserHelperClass(name, username, email, phoneNo, password);
-            }
-            reference.child(username).setValue(helperClass);
-            Toast.makeText(RegisterActivity.this,"Successfully Registered As Seller",Toast.LENGTH_LONG).show();
+            register("Seller");
         });
     }
-
+//check for correct data keyed into the textfield
     public boolean checkAll(String username, String name, String contact,
                             String email, String pwd) {
         String error = "";
@@ -91,5 +79,45 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(RegisterActivity.this, error, Toast.LENGTH_SHORT).show();
             return false;
         }
+    }
+    public void register(String path){
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference(path);//register as Seller or Buyer
+        // get all the values from the textfield
+        String username = et_username.getText().toString();
+        String name = et_name.getText().toString();
+        String email = et_email.getText().toString();
+        String phoneNo = et_contact.getEditableText().toString();
+        String password = et_password.getEditableText().toString();
+        //register into database if parameter is correct
+        if (checkAll(username, name, phoneNo, email, password)) {
+            //query database to check for username
+            queryRepeatedUsername(username,name,phoneNo,email,password,path);
+        }
+    }
+
+    public void queryRepeatedUsername(String username,String name,String phoneNo,String email,String password,String path){
+        Query checkUser = reference.orderByChild("username").equalTo(username);
+        checkUser.addValueEventListener(new ValueEventListener(){
+           @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){//if username exist in database,set an error
+                    et_username.setError("Username already exists\nPlease try a different one");
+                }
+                else{//otherwise if its a new username run this codes below
+                    et_username.setError(null);
+                    UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNo, password);
+                    reference.child(username).setValue(helperClass);
+                    Toast.makeText(RegisterActivity.this,"Successfully Registered As "+path,Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("getActivity()",error.getMessage());
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/mad_project/fragments/SellerAddFragment.java
+++ app/src/main/java/com/example/mad_project/fragments/SellerAddFragment.java
@@ -3,18 +3,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.mad_project.R;
 
 public class SellerAddFragment extends Fragment {
+    Spinner spinner;
     public SellerAddFragment() {
         super(R.layout.fragment_seller_add);
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_seller_add, container, false);
+        //spinner
+        spinner=(Spinner)view.findViewById(R.id.product_category);
+        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(getActivity(),R.array.category,
+                android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         return view;
     }
 }
